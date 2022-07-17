d3468e788d95a6534a4e2e9a42bcc9fb19035ecf
Leo The Lion <105739936+leomorenodevelops@users.noreply.github.com>
2022-07-17 16:47:06 -0600

    Completed js commenting

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,23 @@
 var today = moment();
 
+// Function will run after HTML document is rendered.
 $(document).ready(function() {
+
+    // Targeting id element to have it display current day of the week followed by month then day of the month.
     $('#currentDay').text(today.format("dddd, MMMM Do"));
     
+    // Event listener for save button on click.
     $('.saveBtn').on('click', function() {
+
+        // Event tareting saveBtn sibling <text area> class description value.
         var eventText = $(this).siblings('.description').val();
+
+        // Event targeting saveBtn parent <div> id element.
         var time = $(this).parent().attr('id');
         localStorage.setItem(time, eventText);
     })
-    // document.querySelectorAll(".saveBtn").addEventListener("click", function(event) {
-    //     var thisEl = event.target;
-    //     var eventText = thisEl.previousSiblingElement.value;
-    //     var time = "hour-9";
-    //     localStorage.setItem("hour-9", "Breakfast");
-    // })
-
-
-    // your code for loading your localstroage values, and then rendering them into their corresponding textareas
+    
+    // Code for loading your localstroage key values, and then rendering them into their corresponding textareas
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
     $('#hour-10 .description').val(localStorage.getItem('hour-10'));
     $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -27,13 +28,15 @@ $(document).ready(function() {
     $('#hour-16 .description').val(localStorage.getItem('hour-16'));
     $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
+    // Function for taking current hour.
     function hourChange() {
         var currentHour = today.hour();
+        
+        // function for targeting time block class spliting the id element and returning it as an integer.
         $('.time-block').each(function() {
             var eventHour = parseInt($(this).attr('id').split('-')[1]);
-            console.log(eventHour);
-            console.log(currentHour);
-            console.log("===");
+            
+            // Arguement to determine which classes will be added/removed.
             if (eventHour < currentHour) {
                 $(this).addClass('past');
 
@@ -48,5 +51,6 @@ $(document).ready(function() {
         })
     }
 
+    // Function call
     hourChange();
 })
